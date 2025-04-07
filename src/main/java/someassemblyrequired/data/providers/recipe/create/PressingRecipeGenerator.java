5dfff9d5d5e0abb8ba69039cba5ef8223c36bee4
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2025-04-07 20:01:04 +0200

    Update 5.0.0_final(1)_FINAL.pdf

--- src/main/java/someassemblyrequired/data/providers/recipe/create/PressingRecipeGenerator.java
+++ src/main/java/someassemblyrequired/data/providers/recipe/create/PressingRecipeGenerator.java
@@ -6,11 +6,14 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.common.crafting.DataComponentIngredient;
 import someassemblyrequired.SomeAssemblyRequired;
 import someassemblyrequired.integration.ModCompat;
 import someassemblyrequired.integration.farmersdelight.FarmersDelightCompat;
 import someassemblyrequired.item.sandwich.SandwichContents;
+import someassemblyrequired.item.sandwich.SandwichItem;
 import someassemblyrequired.registry.ModDataComponents;
 import vectorwing.farmersdelight.common.registry.ModItems;
 
@@ -21,14 +24,21 @@ public class PressingRecipeGenerator extends ProcessingRecipeGenerator {
     public PressingRecipeGenerator(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> registries) {
         super(packOutput, registries);
 
-        ResourceLocation id = SomeAssemblyRequired.id(BuiltInRegistries.ITEM.getKey(ModItems.HAMBURGER.get()).getPath());
+        stompSandwich(ModItems.HAMBURGER.get(), FarmersDelightCompat.createBurger());
+        stompSandwich(ModItems.BACON_SANDWICH.get(), FarmersDelightCompat.createBLT());
+        stompSandwich(ModItems.EGG_SANDWICH.get(), SandwichItem.makeSandwich(ModItems.FRIED_EGG.get(), ModItems.FRIED_EGG.get()));
+        stompSandwich(ModItems.CHICKEN_SANDWICH.get(), SandwichItem.makeSandwich(someassemblyrequired.registry.ModItems.CHOPPED_CARROT.get(), ModItems.COOKED_CHICKEN_CUTS.get(), ModItems.CABBAGE_LEAF.get()));
+    }
+
+    private void stompSandwich(Item result, ItemStack sandwich) {
+        ResourceLocation id = SomeAssemblyRequired.id(BuiltInRegistries.ITEM.getKey(result).getPath());
         create(id, builder -> {
-                    builder.output(ModItems.HAMBURGER.get());
+                    builder.output(result);
                     return builder.whenModLoaded(ModCompat.FARMERSDELIGHT)
                             .require(DataComponentIngredient.of(false,
                                     ModDataComponents.SANDWICH_CONTENTS,
-                                    SandwichContents.get(FarmersDelightCompat.createBurger()),
-                                    FarmersDelightCompat.createBurger().getItem()
+                                    SandwichContents.get(sandwich),
+                                    sandwich.getItem()
                             ));
                 }
         );
